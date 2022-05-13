13c14d9b16613473fa31d69b298b7b76e2d4fc11
nipun991 <104301573+nipun991@users.noreply.github.com>
2022-05-13 21:38:34 +0530

    Add files via upload
    SQL Data Base File

---
+++ powercut.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2022 at 07:42 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `powercut`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `powercuttime`
+--
+
+CREATE TABLE IF NOT EXISTS `powercuttime` (
+  `PcutID` int(6) NOT NULL AUTO_INCREMENT,
+  `City_ID` varchar(40) NOT NULL,
+  `Province` varchar(60) NOT NULL,
+  `Town` varchar(60) NOT NULL,
+  `Time` varchar(20) NOT NULL,
+  PRIMARY KEY (`PcutID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
